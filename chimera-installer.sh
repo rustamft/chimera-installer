b0c72f778528e6766a3e4a3700f3fd52e91376a4
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2025-04-01 01:06:23 +0500

    Update chimera-installer.sh

--- chimera-installer.sh
+++ chimera-installer.sh
@@ -23,10 +23,16 @@ if [[ $disk == *"nvme"* ]]; then
 fi
 disk_partition_1="${disk}${partition_number_prefix}1"
 disk_partition_2="${disk}${partition_number_prefix}2"
-while [[ -z $password ]] || [[ $password != $password_confirmation ]]; do
-  read -s -p "Enter a password for the ${disk_partition_2} partition encryption: " password
+while [[ -z $password_encryption ]] || [[ $password_encryption != $password_encryption_confirmation ]]; do
+  read -s -p "Enter a password for the ${disk_partition_2} partition encryption: " password_encryption
   printf "\n"
-  read -s -p "Please repeat to confirm: " password_confirmation
+  read -s -p "Please repeat to confirm: " password_encryption_confirmation
+  printf "\n"
+done
+while [[ -z $password_root ]] || [[ $password_root != $password_root_confirmation ]]; do
+  read -s -p "Enter the root password: " password_root
+  printf "\n"
+  read -s -p "Please repeat to confirm: " password_root_confirmation
   printf "\n"
 done
 while [[ -z $is_swap_required ]]; do
@@ -59,19 +65,23 @@ w
 q
 EOF
 mkfs.vfat /dev/$disk_partition_1
-echo -n $password | cryptsetup luksFormat /dev/$disk_partition_2 -
-echo -n $password | cryptsetup luksOpen /dev/$disk_partition_2 cryptroot -
+echo -n $password_encryption | cryptsetup luksFormat /dev/$disk_partition_2 -
+echo -n $password_encryption | cryptsetup luksOpen /dev/$disk_partition_2 cryptroot -
 mkfs.f2fs /dev/mapper/cryptroot
 if [[ ! -e /dev/$disk_partition_1 ]] || [[ ! -e /dev/$disk_partition_2 ]] || [[ ! -e /dev/mapper/cryptroot ]]; then
   echo "${disk} is not partitioned correctly"
   exit
 fi
-mkdir -p /media/root/boot
+mkdir -p /media/root/boot/efi
 mount /dev/mapper/cryptroot /media/root
 mount /dev/$disk_partition_1 /media/root/boot
 chmod 755 /media/root
 chimera-bootstrap /media/root
 chimera-chroot /media/root << EOF
-apk add linux-$kernel_type
+echo -n $password_root | passwd --stdin root
+apk add linux-$kernel_type grub-x86_64-efi
 genfstab / >> /etc/fstab
+update-initramfs -c -k all
+grub-install --efi-directory=/boot/efi
+update-grub
 EOF
