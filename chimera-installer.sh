a22ca22955852d912e675c2983d6a831d48528af
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2025-04-06 23:05:02 +0500

    Update chimera-installer.sh

--- chimera-installer.sh
+++ chimera-installer.sh
@@ -173,11 +173,11 @@ chimera-chroot /media/root << EOF
 echo -n ${password_admin} | passwd --stdin root
 useradd -G wheel,kvm,plugdev $user_name
 echo -n ${password_admin} | passwd --stdin ${user_name}
-echo $host_name > /etc/hostname
+echo ${host_name} > /etc/hostname
 echo y | apk add chimera-repo-user
 apk update
 echo y | apk add bash grub-x86_64-efi cryptsetup-scripts dbus networkmanager bluez pipewire xserver-xorg-minimal ${packages}
-chsh -s /bin/bash $user_name
+chsh -s /bin/bash ${user_name}
 dinitctl -o enable networkmanager
 dinitctl -o enable bluetoothd
 case $desktop_environment in
