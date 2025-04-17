35cd72dcf48fc26598999e7510116a054e794346
Rustam Tazyrov <81812929+rustamft@users.noreply.github.com>
2025-04-17 22:43:51 +0500

    Update chimera-installer.sh

--- chimera-installer.sh
+++ chimera-installer.sh
@@ -19,115 +19,117 @@ echo ""
 # User choices
 
 while [ -z $disk ] || [ ! -e /dev/$disk ]; do
-  read -p "Enter a valid disk name (e.g. sda or nvme0n1): " disk
+  read -p 'Enter a valid disk name (e.g. sda or nvme0n1): ' disk
 done
-if [ $disk == *"nvme"* ]; then
-  partition_number_prefix="p"
+if [ $disk == *'nvme'* ]; then
+  partition_number_prefix='p'
 fi
 disk_partition_1="${disk}${partition_number_prefix}1"
 disk_partition_2="${disk}${partition_number_prefix}2"
 while [ -z $password_encryption ] || [ $password_encryption != $password_encryption_confirmation ]; do
-  read -s -p "Enter a password for the ${disk_partition_2} partition encryption: " password_encryption
-  printf "\n"
-  read -s -p "Please repeat to confirm: " password_encryption_confirmation
-  printf "\n"
+  stty -echo; IFS= read -r -p 'Enter a password for the ${disk_partition_2} partition encryption: ' password_encryption; stty echo
+  printf '\n'
+  stty -echo; IFS= read -r -p 'Please repeat to confirm: ' password_encryption_confirmation; stty echo
+  printf '\n'
 done
+unset password_encryption_confirmation
 while [ -z $user_name ]; do
-  read -p "Enter a new administrator name: " user_name
+  read -p 'Enter a new administrator name: ' user_name
 done
 while [ -z $password_admin ] || [ $password_admin != $password_admin_confirmation ]; do
-  read -s -p "Enter the administrator password (also used for the root): " password_admin
-  printf "\n"
-  read -s -p "Please repeat to confirm: " password_admin_confirmation
-  printf "\n"
+  stty -echo; IFS= read -r -p 'Enter the administrator password (also used for the root): ' password_admin; stty echo
+  printf '\n'
+  stty -echo; IFS= read -r -p 'Please repeat to confirm: ' password_admin_confirmation; stty echo
+  printf '\n'
 done
+unset password_admin_confirmation
 while [ -z $host_name ]; do
-  read -p "Enter the host name: " host_name
+  read -p 'Enter the host name: ' host_name
 done
-while [ $processor_type != "amd" ] && [ $processor_type != "intel" ]; do
-  printf "Choose processor type:\n  1) AMD\n  2) Intel\n"
+while [ $processor_type != 'amd' ] && [ $processor_type != 'intel' ]; do
+  printf 'Choose processor type:\n  1) AMD\n  2) Intel\n'
   read processor_type
   case $processor_type in
-    "1")
-      processor_type="amd"
+    '1')
+      processor_type='amd'
       packages="$packages ucode-amd"
       ;;
-    "2")
-      processor_type="intel"
+    '2')
+      processor_type='intel'
       packages="$packages ucode-intel"
       ;;
     *)
-      printf "This is not an option\n"
+      printf 'This is not an option\n'
       unset processor_type ;;
   esac
 done
-while [ $kernel_type != "lts" ] && [ $kernel_type != "stable" ]; do
-  printf "Choose kernel type:\n  1) LTS\n  2) Stable\n"
+while [ $kernel_type != 'lts' ] && [ $kernel_type != 'stable' ]; do
+  printf 'Choose kernel type:\n  1) LTS\n  2) Stable\n'
   read kernel_type
   case $kernel_type in
-    "1")
-      kernel_type="lts"
+    '1')
+      kernel_type='lts'
       packages="$packages linux-lts"
       ;;
-    "2")
-      kernel_type="stable"
+    '2')
+      kernel_type='stable'
       packages="$packages linux-stable"
       ;;
     *)
-      printf "This is not an option\n"
+      printf 'This is not an option\n'
       unset kernel_type ;;
   esac
 done
 while [ -z $desktop_environment ]; do
-  printf "Choose desktop environment:\n  1) None\n  2) Minimal GNOME\n  3) KDE\n"
+  printf 'Choose desktop environment:\n  1) None\n  2) Minimal GNOME\n  3) KDE\n'
   read desktop_environment
   case $desktop_environment in
-    "1")
-      desktop_environment="none" ;;
-    "2")
-      desktop_environment="gnome"
+    '1')
+      desktop_environment='none' ;;
+    '2')
+      desktop_environment='gnome'
       packages="$packages gdm gnome-desktop xdg-desktop-portal-gnome gnome-shell-extensions gnome-system-monitor gnome-color-manager gnome-tweaks tlp kitty file-roller nautilus"
       ;;
-    "3")
-      desktop_environment="kde"
+    '3')
+      desktop_environment='kde'
       packages="$packages sddm plasma-desktop tlp kitty"
       ;;
     *)
-      printf "This is not an option\n"
+      printf 'This is not an option\n'
       unset desktop_environment ;;
   esac
 done
 while [ -z $is_flatpak_required ]; do
-  read -p "Is Flatpak installation required? [Y/n] " is_flatpak_required
+  read -p 'Is Flatpak installation required? [Y/n] ' is_flatpak_required
   case $is_flatpak_required in
-    ""|"Y"|"y")
+    ''|'Y'|'y')
       is_flatpak_required=true
       packages="$packages flatpak"
       ;;
-    "N"|"n")
+    'N'|'n')
       is_flatpak_required=false ;;
     *)
-      printf "This is not an option\n"
+      printf 'This is not an option\n'
       unset is_flatpak_required
       ;;
   esac
 done
 while [ -z $is_swap_required ]; do
-  read -p "Would you like Swap file and zRAM device to be configured? [Y/n] " is_swap_required
+  read -p 'Would you like Swap file and zRAM device to be configured? [Y/n] ' is_swap_required
   case $is_swap_required in
-    ""|"Y"|"y")
+    ''|'Y'|'y')
       is_swap_required=true
       while ! [ $swap_size -eq $swap_size 2>/dev/null -o $swap_size -eq 0 2>/dev/null ]; do
-        read -p "Swap size (Gb): " swap_size
+        read -p 'Swap size (Gb): ' swap_size
       done
       while ! [ $zram_size -eq $zram_size 2>/dev/null -o $zram_size -eq 0 2>/dev/null ]; do
-        read -p "zRAM size (Gb): " zram_size
+        read -p 'zRAM size (Gb): ' zram_size
       done
       ;;
-    "N"|"n")
+    'N'|'n')
       is_swap_required=false ;;
     *)
-      printf "This is not an option\n"
+      printf 'This is not an option\n'
       unset is_swap_required
       ;;
   esac
@@ -181,9 +183,9 @@ dinitctl -o enable networkmanager
 dinitctl -o enable bluetoothd
 dinitctl -o enable tlp
 case ${desktop_environment} in
-  "gnome")
+  'gnome')
     dinitctl -o enable gdm ;;
-  "kde")
+  'kde')
     dinitctl -o enable sddm ;;
 esac
 if ${is_flatpak_required}; then
@@ -214,6 +216,8 @@ EOF
 
 # Finalizing
 
+unset password_encryption
+unset password_admin
 umount -R /media/root
 cryptsetup luksClose /dev/mapper/cryptroot
 cat << EOF
